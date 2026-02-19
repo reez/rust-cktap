a1e41d80fcb91b7aa1490058f0dd3385b9827212
Matthew <ramsden@hey.com>
2026-02-19 11:47:54 -0600

    fix(swiftpm): use tools 5.9 and iOS "18.0" in package manifests

--- cktap-swift/Package.swift
+++ cktap-swift/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "cktap-swift",
     platforms: [
         .macOS(.v12),
-        .iOS(.v18)
+        .iOS("18.0")
     ],
     products: [
         .library(
